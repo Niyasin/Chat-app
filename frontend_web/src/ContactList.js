577d69e60a55585d7093da7cc0c7a18087063836
Niyasin <mo.niyas2000@gmail.com>
2022-11-13 23:55:48 +0530

    ContactList Component 👥

---
+++ frontend_web/src/ContactList.js
@@ -0,0 +1,50 @@
+import { useEffect, useState } from "react";
+const ContactList=(prop)=>{
+    const[search,setSearch]=useState(false);
+    const[contacts,setContacts]=useState([
+        {
+            displayname:'niyas',
+            username:'niyasin',
+            profilePic:'./images/unknown.jpg'
+        },
+    ]);
+
+    return(
+        <div className="contactContainer">
+        <div className="nav_contacts">
+            <h1>Contacts</h1>
+            <input className="input2" placeholder="Search"></input>
+        </div>
+        <div className="listItems">
+            {!search?contacts.map((C)=>{
+                return(
+                    <ContactListItem
+                                key={C.username}
+                                contact={C}
+                                onClick={()=>{
+                                }}/>
+                );
+            }):search.map((C)=>{
+                return(
+                    <ContactListItem 
+                                contact={C}
+                                onClick={(e)=>{}}
+                                />
+                )
+            })}
+
+        </div>
+        </div>
+    );
+}
+const ContactListItem=(props)=>{
+    return(
+        <div className="ContactListItem" onClick={props.onClick}>
+            <img className="profile1x1" src={props.contact.profilePic} draggable='false'/>
+            <h3>{props.contact.displayname}</h3>
+            <span>@{props.contact.username}</span>
+        </div>
+    );
+}
+
+export default ContactList;
--- frontend_web/src/Container.js
+++ frontend_web/src/Container.js
@@ -1,11 +1,16 @@
 import { useState ,useEffect } from "react";
 import Login from "./Login.js";
+import ContactList from "./ContactList.js";
+// import Chat from "./Chat.js";
 const Container=()=>{
-    let [user,setUser]=useState(false);    
+    let [user,setUser]=useState(true);    
     return(
         <>
         {!user?<Login/>:
         <div className="container">
+            <div>User info</div>
+            <ContactList/>
+            <div>Chat Component</div>
         </div>}
         </>
     );
