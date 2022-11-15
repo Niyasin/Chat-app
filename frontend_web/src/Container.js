f599438f8decc1f9921a8dcd5641ad94dccb6202
Niyasin <mo.niyas2000@gmail.com>
2022-11-15 14:41:04 +0530

    UserInfo Component 🔽

--- frontend_web/src/Container.js
+++ frontend_web/src/Container.js
@@ -3,13 +3,25 @@ import Login from "./Login.js";
 import ContactList from "./ContactList.js";
 // import Chat from "./Chat.js";
 const Container=()=>{
-    let [user,setUser]=useState(true);    
+    let [user,setUser]=useState({
+        username:'Niyas',
+        displayname:'Niyas',
+        profilePic:'./images/unknown.jpg',
+    });    
     return(
         <>
+       
         {!user?<Login/>:
         <div className="container">
-            <div>User info</div>
             <ContactList/>
+            <div className="userInfo">
+            <img className="profile1x1"
+                     src={user.profilePic}
+                     onClick={''}
+                     />
+            <h2>{user.displayname}</h2>
+            <div className="btn" onClick={()=>{}}>Logout</div>
+        </div>
             <div>Chat Component</div>
         </div>}
         </>
