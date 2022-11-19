e58881d542763e50e3c2e40b686711fe367ed0a6
Niyasin <mo.niyas2000@gmail.com>
2022-11-19 15:27:40 +0530

    Image Message 📷

--- frontend_web/src/Chat.js
+++ frontend_web/src/Chat.js
@@ -36,57 +36,96 @@ const Chat=(props)=>{
     })
     const send=()=>{
         let m={
-            type:'text',
             to:props.contact.username,
-            data:inputData,
+        }
+        if(file){
+            m.type='image';
+            m.data=file;
+            m.text=inputData;
+        }else{
+            m.type='text';
+            m.data=inputData;
         }
         socket.emit('privateMessage',m);
         m.from=props.user.username;
         setData(data.concat(m));
         inputField.current.value='';
     }
-    return(
-        <div className="chatContainer">
-        {props.contact?<> 
-        <div className="nav_chat">
-            <img className="profile1x1" src={props.contact.profilePic}></img>
-            <h2>{props.contact.displayname}</h2>
-        </div>
-        <div ref={messages} className="messages">
-            {
-                data.map(m=>{
-                    return(
-                        <Message
-                            type={m.type}
-                            user={m.from==props.user.username?'send':'recieved'}
-                            data={m.data} 
-                            text={m.text?m.text:''}
-                            />
-                    )
-                })
-            }
-            <span ref={bottomRef}></span>
-        </div>
 
-        <div className="chatInput">
-            <input 
-                ref={inputField}
-                className="input3"
-                onInput={(e)=>{setInputData(e.target.value)}}
-                onKeyDown={(e)=>{if(e.key==='Enter'){send()}}}
-                />
-            <div className="btn2"  onClick={()=>{send()}}>&#10148;</div>
-            <div className="btn2" onClick={()=>{
+    const handleDrop=(event)=>{
+        setFile(false);
+        event.preventDefault();
+        if(event.dataTransfer.files[0].type.substring(0,5)=='image' && event.dataTransfer.files[0].size<15000000){
+            let reader=new FileReader();
+            reader.onloadend=()=>{
+                setFile(reader.result);
+            }
+            reader.readAsDataURL(event.dataTransfer.files[0]);
+        }
+    };
+
+    const handleBrowse=()=>{
+        let input = document.createElement('input');
+        input.type='file';
+        input.onchange=()=>{
+            if(input.files[0].type.substring(0,5)=='image' && input.files[0].size<15000000){
+                let reader=new FileReader();
+                reader.onloadend=()=>{
+                    setFile(reader.result);
+                }
+                reader.readAsDataURL(input.files[0]);
+            }
+        }
+        input.click();
+    }
+
+    return(
+        <>
+        <div className="chatContainer">
+            {props.contact?<> 
+            <div className="nav_chat">
+                <img className="profile1x1" src={props.contact.profilePic}></img>
+                <h2>{props.contact.displayname}</h2>
+            </div>
+            <div ref={messages} className="messages">
+                {
+                    data.map((m,i)=>{
+                        return(
+                            <Message
+                                key={i}
+                                type={m.type}
+                                user={m.from==props.user.username?'send':'recieved'}
+                                data={m.data} 
+                                text={m.text?m.text:''}
+                                />
+                        )
+                    })
+                }
+                <span ref={bottomRef}></span>
+            </div>
+
+            <div className="chatInput">
+                <input 
+                    ref={inputField}
+                    className="input3"
+                    onInput={(e)=>{setInputData(e.target.value)}}
+                    onKeyDown={(e)=>{if(e.key==='Enter'){send()}}}
+                    />
+                <div className="btn2"  onClick={()=>{send()}}>&#10148;</div>
+                <div className="btn2" onClick={()=>{
                                                     setFile(null);
-                                                    fileIn?setFileIn(false):setFileIn(true)
+                                                    setFileIn(true);
                                                     }}
+                                        
                                                     >&#10064;</div>
+            </div>
+            </>:<>
+            <img src="images/blank.png" className="chatAltImage" draggable='false'/>
+            </>}
         </div>
-        </>:<>
-        <img src="images/blank.png" className="chatAltImage" draggable='false'/>
-        </>}
-        </div>
-    );
+        {fileIn?<FileInput setFileIn={setFileIn} setFile={setFile} handleBrowse={handleBrowse} handleDrop={handleDrop} file={file}/>:''}
+    </>
+        );
 }
 export default Chat;
 
@@ -111,4 +150,29 @@ const Message=(props)=>{
             </div>
             )
     }
+}
+
+const FileInput=(props)=>{
+    return(
+        <div className="fileInput" 
+                onDragEnter={(event)=>{event.preventDefault()}}
+                onDragOver={(event)=>{event.preventDefault()}}
+                onDrop={(event)=>{props.handleDrop(event)}}
+        >
+            {props.file?
+            <>
+                <img src={props.file} className='imagePreview1'/>
+                <div className="btn" onClick={()=>{props.setFileIn(false);props.setFile(null)}}>Cancel</div>
+            </>
+            :<>
+            <img src="./images/drop.png" className="symbol"/>
+            <h2>Drag Drop File Here</h2>
+            <div className="horizontal">
+                <div className="btn" onClick={props.handleBrowse}>Browse File</div>
+                <div className="btn" onClick={()=>{props.setFileIn(false);props.setFile(null)}}>Cancel</div>
+            </div>
+            </>
+            }
+        </div>
+    );
 }
