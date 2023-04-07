ee0a8cbff7c63c0d23cdabf9ec2c9d69e8d4b209
Niyasin <mo.niyas2000@gmail.com>
2023-04-07 11:20:45 +0530

    navigation and fonts

--- frontend_reactnative/App.js
+++ frontend_reactnative/App.js
@@ -1,17 +1,30 @@
 import { StyleSheet, Text, View} from 'react-native';
-import {useFonts} from 'expo-font';
+import * as Font from 'expo-font';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from './Screens/Home';
+import Chat from './Screens/Chat';
+import { useEffect } from 'react';
 
-import Login from './Login'
 
-const url='http://localhost:8080/'
+const Stack = createNativeStackNavigator();
+const loadFont =async()=>{
+  await Font.loadAsync('Poppins-reqular',require('./assets/Poppins-Regular.ttf'));
+  await Font.loadAsync('Poppins-bold',require('./assets/Poppins-Bold.ttf'));
+}
+
+
 
 export default function App() {
-  const Cairo = useFonts({
-    'Cairo':require('./assets/Cairo.ttf'),
-  });
+  useEffect(()=>{loadFont()},[]);
   return (
     <View style={styles.container}>
-      <Login url={url}/>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen component={Home} name='Home'/>
+          <Stack.Screen component={Chat} name='Chat'/>
+        </Stack.Navigator>
+      </NavigationContainer>
     </View>
   );
 }
---
+++ frontend_reactnative/Screens/Home.js
@@ -0,0 +1,18 @@
+import { SafeAreaView ,StyleSheet} from "react-native";
+
+export default function Home({natigation,route}){
+    return(
+        <SafeAreaView style={styles.container}>
+
+        </SafeAreaView>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
