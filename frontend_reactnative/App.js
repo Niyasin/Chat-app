44731df44dbbe3f9092a7ef7439815e4d332e609
Niyasin <mo.niyas2000@gmail.com>
2023-04-11 09:12:09 +0530

    splash screen waiting

--- frontend_reactnative/App.js
+++ frontend_reactnative/App.js
@@ -1,33 +1,58 @@
-import { StyleSheet, Text, View} from 'react-native';
+import { StyleSheet,SafeAreaView} from 'react-native';
 import * as Font from 'expo-font';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import * as SplashScreen from 'expo-splash-screen';
+
 import Home from './Screens/Home';
 import Chat from './Screens/Chat';
 import Login from './Screens/Login';
 import {ORIGIN} from './config'
 
-import { useEffect ,useState} from 'react';
+import { useCallback, useEffect ,useState} from 'react';
 import Preview from './Screens/Preview';
 
 const Stack = createNativeStackNavigator();
-const loadFont = async()=>{
-  await Font.loadAsync({
-    'Poppins-regular':require('./assets/Poppins-Regular.ttf'),
-    'Poppins-bold':require('./assets/Poppins-Bold.ttf'),
-  })
-}
+SplashScreen.preventAutoHideAsync();
+
 
 
 
 export default function App() {
   const [user, setUser] = useState(null);
   const [token,setToken]=useState(null);
-  useEffect(()=>{loadFont()},[]);
+  const [ready,setReady]=useState(false);
 
+  
+  useEffect(()=>{
+    async function loadFont (){
+      try{
+        await Font.loadAsync({
+          'Poppins-regular':require('./assets/Poppins-Regular.ttf'),
+          'Poppins-bold':require('./assets/Poppins-Bold.ttf'),
+        })
+      }catch(e){
+        console.log(e);
+      }finally{
+        setReady(true)
+      }
+    }
+    loadFont();
+  },[]);
+  
+  const onLayoutRootView = useCallback(async () => {
+    if (ready) {
+      await SplashScreen.hideAsync();
+    }
+  }, [ready]);
+
+  if (!ready) {
+    return null;
+  }
 
   return (
-  <>{user?
+    <SafeAreaView onLayout={onLayoutRootView} style={{flex:1}}>
+      {user?
       <NavigationContainer>
         <Stack.Navigator>
           <Stack.Screen component={Home} initialParams={{user,token}} name='Home' options={{title:'Contacts'}}/>
@@ -36,8 +61,8 @@ export default function App() {
         </Stack.Navigator>
       </NavigationContainer>
     :
-      <Login setToken={setToken} setUser={setUser}/>
-  }</>
+    <Login setToken={setToken} setUser={setUser}/>
+  }</SafeAreaView>
   );
 }
 const styles = StyleSheet.create({
